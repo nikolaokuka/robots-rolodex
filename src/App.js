eb23cf42fc7788a49cc2364162e3a8bc88ca9bee
nik <nikolaokuka@gmail.com>
2022-04-27 10:10:31 +0200

    fetch robots

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
+  const [robots, setRobots] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => setRobots(users));
+  }, []);
+
   return (
-    <div>Robots Rolodex</div>
+    <div>
+      {robots.map((robot) => (
+        <h1 key={robot.id}>{robot.name}</h1>
+      ))}
+    </div>
   );
 };
 
